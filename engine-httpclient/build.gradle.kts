02b32d32e8603355ecabdc069b11133299b22fad
Pierre Leresteux <pierre.leresteux@gmail.com>
2019-12-23 17:10:46 +0100

    Add sample http

--- engine-httpclient/build.gradle.kts
+++ engine-httpclient/build.gradle.kts
@@ -2,9 +2,11 @@ plugins {
     kotlin("jvm")
 }
 
+val fuelVersion = "2.2.1"
+val kraphVersion = "0.6.0"
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(project(":core"))
-    implementation("com.github.kittinunf.fuel:fuel:2.2.1")
-    implementation("me.lazmaid.kraph:kraph:0.6.0")
+    implementation("com.github.kittinunf.fuel:fuel:$fuelVersion")
+    implementation("me.lazmaid.kraph:kraph:$kraphVersion")
 }
---
+++ samples/http/src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+fun main() {
+    MagicalUpdater().executeChangelog(
+        """
+import dsl.action.HttpScriptAction
+import dsl.changeSet
+import dsl.changelog
+
+changelog {
+    changesets {
+        +changeSet {
+            id = "ChangeSet-Http-1"
+            author = "Postman"
+            actions {
+                +HttpScriptAction {
+                    val (_, _, result) = it.restClient
+                        .get("https://httpbin.org/get")
+                        .responseString()
+                    it.logger.info {result.get()}
+                }
+            }
+        }
+    }
+}
+    """.trimIndent()
+    )
+}
