6dcc36f95cb7e82a6ff694d727b85b30f4ce8b2c
Pierre Leresteux <pierre.leresteux@gmail.com>
2020-01-06 10:46:42 +0100

    Fix license

--- engine-kubernetes/src/main/kotlin/engine/kubernetes/KubernetesEngine.kt
+++ engine-kubernetes/src/main/kotlin/engine/kubernetes/KubernetesEngine.kt
@@ -1,7 +1,7 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
  *
- * Copyright 2019 Pierre Leresteux.
+ * Copyright 2019-2020 Pierre Leresteux.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
