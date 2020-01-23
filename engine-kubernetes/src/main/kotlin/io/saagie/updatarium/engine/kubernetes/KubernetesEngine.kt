8d95c394c5cf1afe2392516cd74eb0d2924ddc05
Pierre Leresteux <pierre.leresteux@gmail.com>
2020-01-23 14:55:43 +0100

    :sparkles: Add sample multiplechangelog-withlabel
    Add tags on changesets
    
    Fix style/lint errors
    
    [BOT] Bump new version

--- engine-kubernetes/src/main/kotlin/io/saagie/updatarium/engine/kubernetes/KubernetesEngine.kt
+++ engine-kubernetes/src/main/kotlin/io/saagie/updatarium/engine/kubernetes/KubernetesEngine.kt
@@ -19,9 +19,7 @@ package io.saagie.updatarium.engine.kubernetes
 
 import io.fabric8.kubernetes.client.DefaultKubernetesClient
 
-class KubernetesEngine {
-    companion object {
-        fun getClient() = DefaultKubernetesClient()
-        fun getClient(namespace: String) = getClient().inNamespace(namespace)
-    }
+object KubernetesEngine {
+    fun getClient() = DefaultKubernetesClient()
+    fun getClient(namespace: String) = getClient().inNamespace(namespace)
 }
