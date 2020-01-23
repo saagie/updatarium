77682663b4e75648bff9185ee7f30c618b58a9ca
Pierre Leresteux <pierre.leresteux@gmail.com>
2020-01-23 14:55:43 +0100

    Add sample multiplechangelog-withlabel

---
+++ samples/basic-multiplechangelog-withlabel/src/main/resources/changelogs/changelog1.kts
@@ -0,0 +1,51 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2019-2020 Pierre Leresteux.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import io.saagie.updatarium.dsl.action.BasicAction
+import io.saagie.updatarium.dsl.changeSet
+import io.saagie.updatarium.dsl.changelog
+
+changelog {
+    changesets {
+        +changeSet {
+            id = "ChangeSet-bash-1-1"
+            author = "Bash"
+            labels = listOf("before")
+            actions {
+                +BasicAction {
+                    (1..5).forEach {
+                        logger.info { "Hello $it!" }
+                    }
+
+                }
+            }
+        }
+        +changeSet {
+            id = "ChangeSet-bash-1-2"
+            author = "Bash"
+            labels = listOf("after")
+            actions {
+                +BasicAction {
+                    (6..10).forEach {
+                        logger.info { "Hello $it!" }
+                    }
+
+                }
+            }
+        }
+    }
+}
